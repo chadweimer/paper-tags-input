6d2ff1bfd134368d6423296220a274028e13b775
Chad Weimer <chadweimer@gmail.com>
2019-01-30 06:56:03 -0800

    Missed one

--- paper-tags-input.js
+++ paper-tags-input.js
@@ -14,86 +14,86 @@ import '@cwmr/paper-chip/paper-chip.js';
 * @demo demo/index.html
 */
 class PaperTagsInput extends PolymerElement {
-  static get template() {
-    return html`
-        <style>
-            :host {
-                display: block;
-            }
-            :host[hidden] {
-                display: none !important;
-            }
-            input {
-                text-transform: lowercase;
-                height: 36px;
-                width: auto !important;
-                padding-left: 0.5em;
-            }
-            paper-chip {
-                margin: 2px;
-                padding-right: 6px;
-                cursor: pointer;
-            }
-            iron-icon {
-                --iron-icon-height: 20px;
-                --iron-icon-width: 20px;
-                color: var(--disabled-text-color);
-            }
-        </style>
-    
-        <paper-input label="Tags" placeholder="+tag" on-keydown="_onInputKeydown">
-            <div slot="prefix">
-                <template is="dom-repeat" items="[[tags]]">
-                    <paper-chip selectable="">[[item]] <iron-icon icon="icons:cancel" on-click="_onTagRemoveClicked"></iron-icon></paper-chip>
-                </template>
-            </div>
-        </paper-input>
+    static get template() {
+        return html`
+            <style>
+                :host {
+                    display: block;
+                }
+                :host[hidden] {
+                    display: none !important;
+                }
+                input {
+                    text-transform: lowercase;
+                    height: 36px;
+                    width: auto !important;
+                    padding-left: 0.5em;
+                }
+                paper-chip {
+                    margin: 2px;
+                    padding-right: 6px;
+                    cursor: pointer;
+                }
+                iron-icon {
+                    --iron-icon-height: 20px;
+                    --iron-icon-width: 20px;
+                    color: var(--disabled-text-color);
+                }
+            </style>
+        
+            <paper-input label="Tags" placeholder="+tag" on-keydown="_onInputKeydown">
+                <div slot="prefix">
+                    <template is="dom-repeat" items="[[tags]]">
+                        <paper-chip selectable="">[[item]] <iron-icon icon="icons:cancel" on-click="_onTagRemoveClicked"></iron-icon></paper-chip>
+                    </template>
+                </div>
+            </paper-input>
 `;
-  }
+    }
 
-  static get is() { return 'paper-tags-input'; }
-  static get properties() {
-      return {
-          tags: {
-              type: Array,
-              notify: true,
-              value: function() { return []; }
-          }
-      };
-  }
+    static get is() { return 'paper-tags-input'; }
+    static get properties() {
+        return {
+            tags: {
+                type: Array,
+                notify: true,
+                value: function() { return []; }
+            }
+        };
+    }
 
-  addTag(tag) {
-      if (this.tags === null) {
-          this.tags = [];
-      }
-      
-      var trimmedTag = tag.replace(/^\s+/, '').replace(/\s+$/, '');
-      if (trimmedTag !== '') {
-          var tagIndex = this.tags.indexOf(trimmedTag);
-          if (tagIndex === -1) {
-              this.push('tags', trimmedTag);
-          }
-      }
-  }
-  removeTag(tag) {
-      if (this.tags === null) {
-          return;
-      }
-      var tagIndex = this.tags.indexOf(tag);
-      if (tagIndex > -1) {
-          this.splice('tags', tagIndex, 1);
-      }
-  }
+    addTag(tag) {
+        if (this.tags === null) {
+            this.tags = [];
+        }
+        
+        var trimmedTag = tag.replace(/^\s+/, '').replace(/\s+$/, '');
+        if (trimmedTag !== '') {
+            var tagIndex = this.tags.indexOf(trimmedTag);
+            if (tagIndex === -1) {
+                this.push('tags', trimmedTag);
+            }
+        }
+    }
+    removeTag(tag) {
+        if (this.tags === null) {
+            return;
+        }
+        var tagIndex = this.tags.indexOf(tag);
+        if (tagIndex > -1) {
+            this.splice('tags', tagIndex, 1);
+        }
+    }
 
-  _onTagRemoveClicked(e) {
-      this.remove(e.model.item);
-  }
-  _onInputKeydown(e) {
-      if (e.keyCode === 13) {
-          this.addTag(e.target.value.toLowerCase());
-          e.target.value = '';
-      }
-  }
+    _onTagRemoveClicked(e) {
+        this.removeTag(e.model.item);
+    }
+    _onInputKeydown(e) {
+        if (e.keyCode === 13) {
+            this.addTag(e.target.value.toLowerCase());
+            e.target.value = '';
+        }
+    }
 }
 
 window.customElements.define(PaperTagsInput.is, PaperTagsInput);
